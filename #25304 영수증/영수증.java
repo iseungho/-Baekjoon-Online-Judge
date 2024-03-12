b031fa1d7cd16bbf7eb1bb408d377b1130fbe5ac
iseungho <112773313+iseungho@users.noreply.github.com>
2024-03-12 15:50:03 +0900

    [Bronze IV 25304] Title: 영수증, Time: 124 ms, Memory: 14192 KB -NKLCBHub

---
+++ #25304 영수증/영수증.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int comp = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            comp += a * b;
+        }
+        if (X == comp) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
